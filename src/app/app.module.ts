238e1ccce5dc1f2a69c5778d139dfb86e4df6f9f
trevin-livele <liveletrevin6@gmail.com>
2022-04-11 13:57:45 +0300

    created the initial components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FindFlightsComponent } from './components/find-flights/find-flights.component';
+import { DisplayFlightsComponent } from './components/display-flights/display-flights.component';
+import { PassengerDetailsComponent } from './components/passenger-details/passenger-details.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindFlightsComponent,
+    DisplayFlightsComponent,
+    PassengerDetailsComponent,
+    ConfirmComponent
   ],
   imports: [
     BrowserModule,
